c5b8849086b01db3099b99e440f140716d1cc7b1
Axel Pröger <a.proeger@gmail.com>
2024-02-29 22:41:33 +0100

    added chicken class

---
+++ classes/chicken.class.js
@@ -0,0 +1,28 @@
+class Chicken extends MovableObject {
+  width = 100;
+  height = 98;
+  x = 300 + Math.random() * 500;
+  y = 330;
+
+  IMAGES_WALKING = [
+    "img/3_enemies_chicken/chicken_normal/1_walk/1_w.png",
+    "img/3_enemies_chicken/chicken_normal/1_walk/2_w.png",
+    "img/3_enemies_chicken/chicken_normal/1_walk/3_w.png",
+  ];
+
+  constructor() {
+    super();
+    this.loadImage("img/3_enemies_chicken/chicken_normal/1_walk/1_w.png");
+    this.loadImages(this.IMAGES_WALKING);
+    this.animate();
+  }
+
+  animate() {
+    setInterval(() => {
+      let i = this.currentImage % this.IMAGES_WALKING.length;
+      let path = this.IMAGES_WALKING[i];
+      this.img = this.imageCache[path];
+      this.currentImage++;
+    }, 100);
+  }
+}
