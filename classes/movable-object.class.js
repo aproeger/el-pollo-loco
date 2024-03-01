be87efb1f51da3a8e7fbfdc65c15daac40504c1a
Axel Pröger <a.proeger@gmail.com>
2024-03-01 23:34:03 +0100

    added gravity and functions

--- classes/movable-object.class.js
+++ classes/movable-object.class.js
@@ -1,28 +1,80 @@
-class MovableObject {
-  x;
-  y;
-  width;
-  height;
-  img;
-  imageCache = {};
-  currentImage = 0;
+class MovableObject extends DrawableObject {
+  speed;
+  speedY = 0;
+  acceleration = 2.5;
+  health = 100;
+  lastHit = 0;
 
-  constructor() {}
-
-  loadImage(path) {
-    this.img = new Image();
-    this.img.src = path;
+  constructor() {
+    super();
   }
 
-  loadImages(arr) {
-    arr.forEach((path) => {
-      let img = new Image();
-      img.src = path;
-      this.imageCache[path] = img;
-    });
+  applyGravity() {
+    setInterval(() => {
+      if (this.isAboveGround() || this.speedY > 0) {
+        this.y -= this.speedY;
+        this.speedY -= this.acceleration;
+      }
+    }, 1000 / 25);
   }
 
-  moveLeft() {}
+  isAboveGround() {
+    return this.y < 135;
+  }
 
-  moveRight() {}
+  drawFrame(ctx) {
+    if (this instanceof Character || this instanceof Chicken || this instanceof Endboss) {
+      ctx.beginPath();
+      ctx.lineWidth = 5;
+      ctx.strokeStyle = "blue";
+      ctx.rect(this.x, this.y, this.width, this.height);
+      ctx.stroke();
+    }
+  }
+
+  moveLeft() {
+    this.x -= this.speed;
+  }
+
+  moveRight() {
+    this.x += this.speed;
+  }
+
+  jump() {
+    this.speedY = 30;
+  }
+
+  hit() {
+    this.health -= 5;
+    if (this.health < 0) {
+      this.health = 0;
+    } else {
+      this.lastHit = new Date().getTime();
+    }
+  }
+
+  isHurt() {
+    let timePassed = new Date().getTime() - this.lastHit;
+    return timePassed < 250;
+  }
+
+  isDead() {
+    return this.health === 0;
+  }
+
+  playAnimation(images) {
+    let i = this.currentImage % images.length;
+    let path = images[i];
+    this.image = this.imageCache[path];
+    this.currentImage++;
+  }
+
+  isColliding(movableObject) {
+    return (
+      this.x + this.width > movableObject.x &&
+      this.y + this.height > movableObject.y &&
+      this.x < movableObject.x &&
+      this.y < movableObject.y + movableObject.height
+    );
+  }
 }
