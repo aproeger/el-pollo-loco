557f4313d9265f5bbe0037e7356da8859e2946f0
Axel Pröger <a.proeger@gmail.com>
2024-03-05 21:07:23 +0100

    added collectable objects to level

--- levels/level1.js
+++ levels/level1.js
@@ -1,7 +1,21 @@
 const level1 = new Level(
   2750,
   [new Chicken(), new Chicken(), new Chicken(), new Endboss()],
-  [new Cloud()],
+
+  [
+    new Bottle(550, 335),
+    new Bottle(950, 335),
+    new Bottle(1450, 335),
+    new Bottle(1850, 335),
+    new Bottle(2350, 335),
+    new Coin(100, 100),
+  ],
+  [
+    new Cloud("img/5_background/layers/4_clouds/1.png", 0),
+    new Cloud("img/5_background/layers/4_clouds/2.png", 852),
+    new Cloud("img/5_background/layers/4_clouds/1.png", 852 * 2),
+    new Cloud("img/5_background/layers/4_clouds/2.png", 852 * 3),
+  ],
   [
     new BackgroundObject("img/5_background/layers/air.png", -852),
     new BackgroundObject("img/5_background/layers/3_third_layer/2.png", -852),
