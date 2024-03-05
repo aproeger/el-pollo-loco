36c1548400b7822569591a20c53a4f9d5cff82d1
Axel Pröger <a.proeger@gmail.com>
2024-03-05 21:10:28 +0100

    added debug, sounds and colliders

--- classes/world.class.js
+++ classes/world.class.js
@@ -3,30 +3,54 @@ class World {
   ctx;
   keyboard;
   cameraX = 0;
-  character = new Character();
+  character = new Character(this);
   level = level1;
-  backgroundObjects = level1.backgroundObjects;
-  statusBar = new StatusBar();
+  debug = true;
+
+  sounds = {
+    background: new Audio("audio/background-music.mp3"),
+  };
+
+  statusBarBottles = new StatusBarBottles();
+  statusBarHealth = new StatusBarHealth();
+  statusBarCoins = new StatusBarCoins();
+  statusBarEndboss = new StatusBarEndboss();
 
   constructor(canvas, keyboard) {
     this.canvas = canvas;
     this.keyboard = keyboard;
     this.ctx = this.canvas.getContext("2d");
     this.draw();
-    this.setWorld();
     this.checkCollisions();
-  }
-
-  setWorld() {
-    this.character.world = this;
+    this.sounds.background.volume = 0.3;
+    this.sounds.background.play();
   }
 
   checkCollisions() {
     setInterval(() => {
-      this.level.enemies.forEach((enemy) => {
-        if (this.character.isColliding(enemy)) {
+      this.level.enemies.forEach((object) => {
+        if (this.character.isColliding(object)) {
           this.character.hit();
-          this.statusBar.setPercentage(this.character.health);
+          this.statusBarHealth.setPercentage(this.character.health);
+        }
+      });
+
+      this.level.collectableObjects.forEach((object, index) => {
+        if (this.character.isColliding(object)) {
+          if (object instanceof Coin) {
+            console.log("coin collected");
+            this.character.collectCoin();
+            console.log("coins:", this.character.coins);
+            this.statusBarCoins.setPercentage(this.character.coins * 20);
+          }
+
+          if (object instanceof Bottle) {
+            console.log("bottle collected");
+            this.character.collectBottle();
+            this.statusBarBottles.setPercentage(this.character.bottles * 20);
+          }
+
+          this.level.collectableObjects.splice(index, 1);
         }
       });
     }, 100);
@@ -38,9 +62,13 @@ class World {
     this.ctx.translate(this.cameraX, 0);
     this.addObjectsToMap(this.level.backgroundObjects);
     this.addObjectsToMap(this.level.clouds);
+    this.addObjectsToMap(this.level.collectableObjects);
 
     this.ctx.translate(-this.cameraX, 0);
-    this.addToMap(this.statusBar);
+    this.addToMap(this.statusBarCoins);
+    this.addToMap(this.statusBarHealth);
+    this.addToMap(this.statusBarBottles);
+    this.addToMap(this.statusBarEndboss);
     this.ctx.translate(this.cameraX, 0);
 
     this.addToMap(this.character);
@@ -66,7 +94,7 @@ class World {
 
     DrawableObject.draw(this.ctx);
 
-    if (DrawableObject instanceof MovableObject) {
+    if (DrawableObject instanceof MovableObject && this.debug) {
       DrawableObject.drawFrame(this.ctx);
     }
 
