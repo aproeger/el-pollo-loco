5658fc88a87678c5332c4d16df05d8160436e550
Axel Pröger <a.proeger@gmail.com>
2024-03-19 00:36:34 +0100

    added sounds and dead animation

--- classes/chicken.class.js
+++ classes/chicken.class.js
@@ -1,9 +1,15 @@
 class Chicken extends MovableObject {
   width = 77;
   height = 75;
-  x = 300 + Math.random() * 500;
+  x = 800 + Math.random() * 500;
   y = 355;
   speed = 0.15 + Math.random() * 0.25;
+  health = 5;
+  damage = 5;
+
+  sounds = {
+    alarm: new Audio("audio/chicken-alarm.mp3"),
+  };
 
   IMAGES_WALKING = [
     "img/3_enemies_chicken/chicken_normal/1_walk/1_w.png",
@@ -11,20 +17,29 @@ class Chicken extends MovableObject {
     "img/3_enemies_chicken/chicken_normal/1_walk/3_w.png",
   ];
 
+  IMAGES_DEAD = ["img/3_enemies_chicken/chicken_normal/2_dead/dead.png"];
+
   constructor() {
     super();
     this.loadImage(this.IMAGES_WALKING[0]);
+    this.loadImages(this.IMAGES_DEAD);
     this.loadImages(this.IMAGES_WALKING);
     this.animate();
   }
 
   animate() {
     setInterval(() => {
-      this.moveLeft();
+      if (!this.isDead()) {
+        this.moveLeft();
+      }
     }, 1000 / 60);
 
     setInterval(() => {
-      this.playAnimation(this.IMAGES_WALKING);
+      if (!this.isDead()) {
+        this.playAnimation(this.IMAGES_WALKING);
+      } else {
+        this.image = this.imageCache[this.IMAGES_DEAD[0]];
+      }
     }, 100);
   }
 }
